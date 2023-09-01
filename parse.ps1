51c92eb16511022f243cb222acd6f09774688698
mithrandyr <mithrandyr@users.noreply.github.com>
2023-08-31 22:02:26 -0400

    more work needed.

--- parse.ps1
+++ parse.ps1
@@ -1,5 +1,6 @@
 param([string]$english)
 
+[SLSentence]::new($english)
 
 
 Class SL {
@@ -20,15 +21,19 @@ Class SL {
     }
 }
 
-class SLAtomic {
+class SLbase {
+    [string]ToString() { throw "Must Overide!" }
+    [string]ToPS() { throw "Must Overide!" }
+}
+class SLAtomic:SLbase {
     [string]$token
     SLAtomic(){}
-    SLAtomic([string]$x){$this.token = $x}
-    [string]ToString() {return $this.token}
-    [string]ToPS() {return '$' + $this.token}
+    SLAtomic([string]$x){ $this.token = $x }
+    [string]ToString() { return $this.token }
+    [string]ToPS() { return '$' + $this.token }
 }
 
-class SLConnective {
+class SLConnective:SLbase {
     $first
     $second #not used for negation
     [string]$connective
@@ -49,7 +54,7 @@ class SLConnective {
         if($this.connective -eq "&") {
             return ("[SL]::And({0}, {1})" -f $this.first.ToPS(), $this.second.ToPS())
         }
-        elseif($this.connective -eq "v") {
+        elseif($this.connective -eq "|") {
             return ("[SL]::Or({0}, {1})" -f $this.first.ToPS(), $this.second.ToPS())
         }
         elseif($this.connective -eq ">") {
@@ -71,29 +76,35 @@ class SLConnective {
 }
 
 class SLSentence {
-    [SLConnective] $MainConnective
+    [SLbase] $MainConnective
     [string[]] $SentenceTokens = @()
     hidden [System.Collections.Generic.Queue[char]]$ParseQueue = [System.Collections.Generic.Queue[char]]::new()
     hidden [string]$theSentence
-    hidden [string]$curIndex = 0
+    hidden [int]$curIndex = 0
+
+    hidden [string[]]$lConnnective = "&","|",">","="
+    hidden [string[]]$lOpenGroup = "(","[","{"
+    hidden [string[]]$lCloseGroup = ")","]","}"
 
     SLSentence([string]$sentence) {
         $Sentence = $Sentence.Replace(" ","").ToUpper()
         $Sentence.ToCharArray().foreach({$null = $this.ParseQueue.Enqueue($_)})
+        $this.theSentence = $Sentence
         if($this.Validate($Sentence)) {
-            $Sentence.MainConnective = $this.ParseQueue()
+            $this.MainConnective = $this.Parse()
+            if($null -eq $this.MainConnective.connective) { $this.MainConnective = $this.MainConnective.first }
         }
         #$this.MainConnective = $this.ParseConnective([SLConnective]::new())
     }
 
     hidden [bool]Validate([string]$sentence) {
         $totalLength = $sentence.Length
-        $connCount = $totalLength - $sentence.Replace("&","|",">","=","")
-        $delimCount = $totalLength - $sentence.Replace("(","[","{","")
+        $connCount = $totalLength - $sentence.Replace("&","").Replace("|","").Replace(">","").Replace("=","").Length
+        $delimCount = $totalLength - $sentence.Replace("(","").Replace("[","").Replace("{","").Length
 
-        if($delimCount -gt $connCount) { thow "too many delimeters" }
-        if($sentence[0] -in "&","|",">","=",")","]","}") { throw "invalid start of sentence" }
-        if($sentence[-1] -in "&","|",">","=","(","[","{","~") { throw "invalid end of sentence" }
+        if($delimCount -gt $connCount) { throw "too many delimeters" }
+        if($sentence[0] -in $this.lConnnective + $this.lCloseGroup) { throw "invalid start of sentence" }
+        if($sentence[-1] -in $this.lConnnective + $this.lOpenGroup + "~") { throw "invalid end of sentence" }
 
         return $true
     }
@@ -104,31 +115,58 @@ class SLSentence {
             $token = $this.theSentence[$this.curIndex]
             $prev = if($this.curIndex -gt 0) { $this.theSentence[$this.curIndex - 1] }
             $next = if($this.curIndex -lt $this.theSentence.Length) { $this.theSentence[$this.curIndex + 1] }
+            $soFar = $this.theSentence[0..$this.curIndex] -join ""
+            $this.curIndex += 1
             
-            #check validity 2nd time
             if($token -eq "~") {
-                if($prev -notin "(","[","{","&","|",">","=" -or $null -ne $prev){
-                    throw "Negation cannot follow '$prev' in '$($this.theSentence[0..$this.curIndex])'"
-                } else {
-                    $nConn = [SLConnective]::new("~")
-                    if($next -in "(","[","{") { $nConn.first = $this.Parse() }
-                    else {
-                        $nConn.first = [SLAtomic]::new($next)
-                        $this.curIndex += 1
-                    }
-                    
-                    if($null -eq $current.first) { $current.first = $nConn }
-                    else { $current.second = $nConn }
+                if($prev -in $this.lCloseGroup -or $prev -match "A-Z"){
+                    throw "Negation cannot follow '$prev' in '$soFar'"
                 }
+
+                # need to rethink this.. maybe support passing in Negation...
+                $nConn = [SLConnective]::new("~")
+                if($next -in $this.lOpenGroup) {
+                    $this.curIndex += 1
+                    $nConn.first = $this.Parse()
+                }
+                elseif($next -eq "~") {
+                    $nConn.first = $this.Parse()
+                }
+                else {
+                    $this.AddSentenceToken($next)
+                    $nConn.first = [SLAtomic]::new($next)
+                    $this.curIndex += 1
+                }
+                
+                if($null -eq $current.first) { $current.first = $nConn }
+                else { $current.second = $nConn }
             }
-            elseif($token -in "&","|",">","="){
-                if($peek -in ")","]","}","&","|",">","=") { throw "Invalid connective, expects Atomic or group"}
+            elseif($token -in $this.lConnnective){
+                if($prev -in $this.lOpenGroup + $this.lConnnective + "~") {
+                    throw "Connective cannot follow '$prev' in '$soFar'"
+                }
                 $current.connective = $token
             }
-            elseif($token -in "(","[","{"){
-
+            elseif($token -in $this.lOpenGroup){
+                if($prev -in $this.lCloseGroup -or $prev -match "A-Z") {
+                    throw "Open Group cannot follow '$prev' in '$soFar'"
+                }
+                $nConn = $this.Parse()
+                $nConn.startDelim = $token
+                if($null -eq $current.first) { $current.first = $nConn }
+                else { $current.second = $nConn }
+            }
+            elseif($token -in $this.lCloseGroup) {
+                if($prev -in $this.lConnnective + $this.lOpenGroup + "~") {
+                    throw "Close Group cannot follow '$prev' in '$soFar'"
+                }
+                elseif($null -eq $current.connective) { throw "Close Group must contain a connective - $soFar" }
+                elseif($null -eq $current.second) { throw "Close group must contain 2 connectives - $soFar" }
+                $current.endDelim = $token
+                return $current
             }
             else {
+                $this.AddSentenceToken($token)
                 if($null -eq $current.first) { $current.first = [SLAtomic]::new($token) }
                 else { $current.second = [SLAtomic]::new($token) }
             }
@@ -197,18 +235,16 @@ class SLSentence {
         return $current
     }
 
-    [string] ToString() {
-        if($this.MainConnective.connective -eq "~") { return $this.MainConnective.ToString() }
-        else {
-            return $this.MainConnective.ToString().SubString(1, $this.MainConnective.ToString().Length -2)    
-        }
-    }
-    
-    AddSentenceToken([string]$t) {
+    hidden AddSentenceToken([string]$t) {
         if(-not $this.SentenceTokens.Contains($t.ToUpper())){
             $this.SentenceTokens += $t
         }
     }
+
+    [string] ToPS() {
+        return $this.MainConnective.ToPS()
+    }
+    
 
     [bool]Invoke([hashtable]$tokenValues) {
         [string]$logicSentence = $tokenValues.Keys.foreach({'$' + $_.ToUpper() + "=$" + [bool]$tokenValues[$_] + "; "}) -join ""
